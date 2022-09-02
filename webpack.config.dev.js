b24cecf318a78c9b815f6f7783012b476a088576
Yohan Manuel Estevez Rueda <estevezruedayohan@gmail.com>
2022-09-02 16:15:15 -0500

    [ADD] Netlify, watch en dev

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,20 +2,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-// const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    clean: true,
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
-  mode: 'production',
+  mode: 'development',
+  watch: true,
   resolve:{
     extensions: ['.js'],
     alias: {
@@ -90,13 +88,5 @@ module.exports = {
     new Dotenv({
       path: './src/.env',
     }),
-    // new CleanWebpackPlugin(),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new TerserPlugin(),
-    ]
-  }
 }
