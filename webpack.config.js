24fd993f39174ee223eab8754ac7ff810009def7
Yohan Manuel Estevez Rueda <estevezruedayohan@gmail.com>
2022-09-02 13:25:28 -0500

    [ADD] fonts

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   resolve:{
     extensions: ['.js']
@@ -33,21 +34,43 @@ module.exports = {
         test: /\.png/,
         type: 'asset/resource'
       },
+      // {
+      //   test: /\.(woff||woff2)$/i,
+      //   use:{
+      //     loader: 'url-loader',
+      //     options: {
+      //       limit: 10000,
+      //       mimetype: 'application/font-woff',
+      //       name: '[name].[ext]',
+      //       outputPath: "./assets/fonts/",
+      //       publicPath: "./assets/fonts/",
+      //       esModule: false,
+      //     },
+      //   },
+      //   type: 'javascript/auto',
+      // },
+      {
+        test: /\.(woff||woff2)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name][ext]',
+        }
+      },
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: './public/index.html',
-      filename: './index.html'
+    new HtmlWebpackPlugin({ // Configuracion del plugin INSTANCIAMOS EL PLUGIN
+      inject: true,         // inyecta el bundle al template HTML
+      template: './public/index.html', // LA RUTA AL TEMPLATE HTML
+      filename: './index.html'          // NOMBRE FINAL DEL ARCHIVO HTML
     }),
     new MiniCssExtractPlugin(),
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src", "assets/images"),
-          to: "assets/images"
-        }
+          from: path.resolve(__dirname, "src", "assets/images"), // Carpeta a mover al dist
+          to: "assets/images"         // ruta final en el dist
+        },
       ]
     }),
   ]
